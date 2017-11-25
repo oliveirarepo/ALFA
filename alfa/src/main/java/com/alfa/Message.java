47b825c1c1495fb8d8a212c6dfde028db031999f
aluno <aluno@LAB15C024.laboratorio.go>
2017-11-25 17:50:31 -0300

    Add Projeto alfa

---
+++ alfa/src/main/java/com/alfa/Message.java
@@ -0,0 +1,15 @@
+package com.alfa;
+
+public class Message {
+	
+	String message = "GCS";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
