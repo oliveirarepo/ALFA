904bba845da922a810ca7d178c3d9b0e64c2286a
oliveirarepo <thiago8596@gmail.com>
2017-11-25 15:55:36 -0300

    Add arquivos no repositório corretamente
    Add arquivos no repositório corretamente

---
+++ src/test/java/alfa/MessageTest.java
@@ -0,0 +1,28 @@
+package alfa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.alfa.Message;
+
+import junit.framework.TestCase;
+
+public class MessageTest extends TestCase {
+
+	@Test
+	public void test() {
+	  
+		Message msg = new Message();
+				
+		String valorEsperado  = "GCS";
+		String valorRetornado = msg.getMessage();
+		
+		assertEquals(valorEsperado, valorRetornado);
+		
+		
+		
+		
+	}
+
+}
