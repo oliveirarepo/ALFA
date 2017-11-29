52dbbfcd2f1e0b1e4be15aacdd899b4350b7fb22
oliveirarepo <thiago8596@gmail.com>
2017-11-29 18:55:40 -0200

    criando classe com erro

---
+++ src/main/java/com/alfa/Erro.java
@@ -0,0 +1,15 @@
+package com.alfa;
+
+public class Erro {
+	
+	String message = "Erro";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
